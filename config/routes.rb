9ea7cba29731d057b287f4ff4a43c1337c518736
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-03 11:10:25 +0300

    Implemented validations for blogs, skills, and portfolio items.

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,11 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  # resources :bloggs do
-  #   member do
-  #     get :toggle_status
-  #   end
-  # end
+  resources :bloggs do
+    member do
+      get :toggle_status
+    end
+  end
 
   resources :bloggs
   resources :posts
